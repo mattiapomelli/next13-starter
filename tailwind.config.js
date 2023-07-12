68ae698eddd14c7189e2a8721c365c1d7d3cbdae
mattiapomelli <mattiapomelli@gmail.com>
2023-07-12 23:25:26 +0200

    Add tailwind forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {},
-  plugins: [require("daisyui")],
+  plugins: [require("@tailwindcss/forms"), require("daisyui")],
   daisyui: {
     themes: [
       {
