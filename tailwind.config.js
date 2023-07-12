577655956c06808db0f5e1b4c3a6ca27dcf0b7ed
mattiapomelli <mattiapomelli@gmail.com>
2023-07-12 17:56:29 +0200

    Add daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  theme: {},
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+          "--btn-text-case": "none",
+          "--rounded-btn": "0.75rem",
+        },
       },
-    },
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+          "--btn-text-case": "none",
+          "--rounded-btn": "0.75rem",
+        },
+      },
+    ],
   },
-  plugins: [],
-}
+};
