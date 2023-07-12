60d8a6416faec54e1f39cbb8ca389c5227a25f6c
mattiapomelli <mattiapomelli@gmail.com>
2023-07-12 21:57:53 +0200

    Add svgr support

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,7 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
+import path from "path";
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -14,6 +16,34 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  webpackFinal: async (config) => {
+    // Support path aliases
+    if (!config.resolve) config.resolve = {};
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../src/"),
+    };
+
+    // This modifies the existing image rule to exclude .svg files
+    // since we want to handle those files with @svgr/webpack
+    const imageRule = config?.module?.rules?.find((rule) => {
+      if (!rule || rule === "...") return false;
+      if (!rule.test) return false;
+      if (!(rule.test instanceof RegExp)) return false;
+      return rule.test.test(".svg");
+    });
+    if (imageRule && imageRule !== "...") {
+      imageRule.exclude = /\.svg$/;
+    }
+
+    // Configure .svg files to be loaded with @svgr/webpack
+    config?.module?.rules?.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
   },
 };
 
--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: [{ loader: "@svgr/webpack", options: { icon: true } }],
+    });
+
+    return config;
+  },
+  ...nextConfig,
+};
