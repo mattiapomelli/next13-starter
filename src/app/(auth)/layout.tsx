670c9390f4386150405643b9fac54b5f8b6d8338
mattiapomelli <mattiapomelli@gmail.com>
2023-07-25 16:13:28 +0200

    Fix button color

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -9,10 +9,9 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
     <Container className="relative flex min-h-screen flex-col items-center justify-center">
       <Link href="/" className="absolute left-4 top-10">
         <Button
-          color="neutral"
           variant="ghost"
           leftIcon={<ArrowLeftIcon className="h-5 w-5" />}
-          className="min-w-0 px-4"
+          className="min-w-0 px-4 text-base-content"
         >
           Back
         </Button>
