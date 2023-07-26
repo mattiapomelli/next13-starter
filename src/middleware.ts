2d157723ecd9834dcec2c7f202b7b07c0f59cc4f
mattiapomelli <mattiapomelli@gmail.com>
2023-07-26 14:49:30 +0200

    Redirect to account if logged in on auth pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,18 @@ import { type NextRequest, NextResponse } from "next/server";
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient({ req, res });
-  await supabase.auth.getSession();
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  const isAuthPage =
+    req.nextUrl.pathname.startsWith("/login") || req.nextUrl.pathname.startsWith("/signup");
+
+  // If user is signed in and the current path is an auth page, redirect the user to /account
+  if (session && isAuthPage) {
+    return NextResponse.redirect(new URL("/account", req.url));
+  }
+
   return res;
 }
