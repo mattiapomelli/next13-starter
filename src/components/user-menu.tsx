f4b048aae3aebf1850c829573f3a9e0e64af9af0
mattiapomelli <mattiapomelli@gmail.com>
2023-07-27 09:36:10 +0200

    Adjust padding on user menu

--- src/components/user-menu.tsx
+++ src/components/user-menu.tsx
@@ -33,13 +33,15 @@ export const UserMenu = ({ user }: UserMenuProps) => {
         <DropdownTrigger className="rounded-full bg-base-200 p-2 hover:bg-base-300">
           <UserIcon className="h-5 w-5" />
         </DropdownTrigger>
-        <DropdownContent className="mt-4">
+        <DropdownContent className="mt-4 p-1.5">
           <div className="mt-1 px-4 text-base-content-neutral">{user.email}</div>
           <hr className="my-2 h-px border-0 bg-base-300" />
-          <DropdownItem href="/account" as={WrappedLink}>
+          <DropdownItem href="/account" as={WrappedLink} className="py-1.5">
             Account
           </DropdownItem>
-          <DropdownItem onClick={handleSignOut}>Sign out</DropdownItem>
+          <DropdownItem onClick={handleSignOut} className="py-1.5">
+            Sign out
+          </DropdownItem>
         </DropdownContent>
       </Dropdown>
     </div>
