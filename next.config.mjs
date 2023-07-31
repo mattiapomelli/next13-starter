d53fd5416cec0af17701da1f61d8a3cfe454b7be
mattiapomelli <mattiapomelli@gmail.com>
2023-07-31 11:27:26 +0200

    Exclude encoding package from build

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,10 @@ import "./src/env.mjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
+    // Exlude encoding from the build, used by Supabase
+    config.externals.push("encoding");
+
+    // Support SVGR
     config.module.rules.push({
       test: /\.svg$/i,
       use: [{ loader: "@svgr/webpack", options: { icon: true } }],
